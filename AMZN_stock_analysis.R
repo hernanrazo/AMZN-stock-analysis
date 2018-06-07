b2f7f61aac4be21ba5c9c7f340779e1f86c3d565
Hernan Razo <hernanrazo@gmail.com>
2018-06-07 15:32:14 -0500

    first commit

---
+++ AMZN_stock_analysis.R
@@ -0,0 +1,33 @@
+dataset <- AMZN
+
+attach(dataset)
+
+#Find mean of all values in Open
+mean(Open)
+
+#Find mean of all values in High
+mean(High)
+
+#Find mean of all values in Low
+mean(Low)
+
+#Find mean of all values in Close
+mean(Close)
+
+#Find mean of all values in Adj Close
+mean(Adj.Close)
+
+#Find mean of all values in Volume
+mean(Volume)
+
+#Find and print day with highest closing stock value
+#Which day had the highest growth between low and high?
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+Amazon stock anaylsis
+===
+
+A simple analysis of amazon's stock history using R. The data used has stock from 5/15/97 to 6/1/18.
+
+
+https://finance.yahoo.com/quote/AMZN/history?period1=863672400&period2=1527915600&interval=1d&filter=history&frequency=1d  
