a20a9da8e589bbf5bd0bbf7d78fe074d6db1f1b5
Hernan Razo <hernanrazo@gmail.com>
2018-06-09 13:59:25 -0500

    modified charts. Added candlestick plot

--- AMZN_stock_analysis.R
+++ AMZN_stock_analysis.R
@@ -5,15 +5,28 @@ library(dplyr)
 
 #get dataset
 #in this case, stock records for AMZN
-dataset <- getSymbols("AMZN", src = "yahoo", from = start, to = end)
+data <- getSymbols("AMZN", 
+                   src = "yahoo", 
+                   from = start, 
+                   to = end, 
+                   auto.assign = F)
+
 start <- as.Date("1997-05-15")
 end <- as.Date("2018-06-01")
 
-#dataset <- data.frame(dataset)
+#Convert data to dataframe
+df <- as.data.frame(data)
 
-setwd("~/Users/hernanrazo/RProjects/AMZN_stock_analysis")
-getwd()
-png(filename="faithful.png")
-plot(AMZN[, "AMZN.Close"], main = "AMZN")
+#make a basic line plot of the closing prices and save the graph
+png(filename="regPlot.png")
+plot(df[,"AMZN.Close"], main = "AMZN")
+dev.off()
+
+#make a basic candlestick plot
+png(filename="candleStickPlot.png")
+candleChart(df, 
+            up.col = "green", 
+            dn.col = "red",
+            theme = "white")
 dev.off()
 
